455dd99f137add53a9ed97d08587d26e182c73da
Shreyas Deshpande <51990143+ShreyasBoss@users.noreply.github.com>
2024-10-21 00:21:37 +0530

    Update ManageBet.js

--- Models/ManageBet.js
+++ Models/ManageBet.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const manageBetSchema = new mongoose.Schema({
   betId: { type: mongoose.Schema.Types.ObjectId, ref: 'Bet', required: true },
-  userIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }],
+  userIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true }],
   amount: { type: Number, required: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
